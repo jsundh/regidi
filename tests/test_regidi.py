5a1bea00a2f384f48f9c763361ba1c4f6d188c7f
Jakob Sundh <jsundh@users.noreply.github.com>
2024-04-28 17:03:07 +0200

    Test all digest18 outputs using sha256

--- tests/test_regidi.py
+++ tests/test_regidi.py
@@ -1,3 +1,5 @@
+import hashlib
+
 import pytest
 
 from regidi import aux5, base6, digest18, digest24
@@ -13,10 +15,20 @@ def test_base6_table():
     assert sorted(aux5) == sorted(set(aux5))
 
 
-def test_digest18():
+def test_digest18_single():
     digest = digest18(122775)
 
     assert digest == "potato"
+
+
+def test_digest18_all():
+    h = hashlib.sha256(usedforsecurity=False)
+    for i in range(0, 2**18):
+        digest = digest18(i)
+        h.update(digest.encode())
+
+    # Expected sha256 digest can be regenerated with: regidi --all | tr -d '\n' | sha256sum
+    assert h.hexdigest() == "d41c3a064bf896a7e5667d6d11e14ac5d99f00d5d57810566a95b662e31fd3e5"
 
 
 class TestDigest24:
